8960a904fc0e7e87e3fd746bb3f115ad60dbba12
Paul Liu <paul@supergood.work>
2020-10-07 16:35:13 -0400

    sublime you jerk

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM ruby:latest
 
 RUN apt-get update \
-	&& apt-get install -y python-pip \
-	&& rm -rf /var/lib/apt/lists/*
+    && apt-get install -y python-pip \
+    && rm -rf /var/lib/apt/lists/*
 
 RUN pip install s3cmd
--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,12 @@ set -euo pipefail
 docker build -t supergood-web:deployenv .
 
 docker run \
-	-it \
-	--rm \
-	--env AWS_ACCESS_KEY_ID \
+    -it \
+    --rm \
+    --env AWS_ACCESS_KEY_ID \
     --env AWS_SECRET_ACCESS_KEY \
     --env AWS_SESSION_TOKEN \
     --env CLOUDFLARE_CLIENT_API_KEY \
     --env CLOUDFLARE_EMAIL \
-	-v "$(pwd)":/app \
-	supergood-web:deployenv /app/deploy_in_docker.sh
+    -v "$(pwd)":/app \
+    supergood-web:deployenv /app/deploy_in_docker.sh
