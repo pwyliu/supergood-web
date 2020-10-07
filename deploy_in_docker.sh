25d40effeb2f1cb489d106e33a0ae6cff03368d0
Paul Liu <paul@supergood.work>
2020-10-07 17:39:05 -0400

    more cache

--- deploy_in_docker.sh
+++ deploy_in_docker.sh
@@ -7,24 +7,34 @@ set -euo pipefail
 working_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ${working_dir}
 
-echo "Bundle install"
-bundle install --deployment
+printf "\n** Running bundle install **\n"
+bundle install --deployment >/dev/null 2>&1
 
-echo "Building site"
+printf "\n** Building site **\n"
 rm -rf build/
 bundle exec middleman build
 
-echo "S3 sync"
+# https://support.cloudflare.com/hc/en-us/articles/200172516
+# https://support.cloudflare.com/hc/en-us/articles/202775670
+printf "\n** S3 sync **\n"
 s3cmd \
-  --quiet \
   --delete-removed \
   --guess-mime-type \
   --no-mime-magic \
   --acl-public \
   --recursive \
+  --add-header="Cache-Control: public, max-age=31536000" \
   sync "build/" "s3://supergood.work/"
 
-echo "Invalidating CDN"
+printf "\n** Invalidating CDN **\n"
 bundle exec middleman cdn
 
-echo "Done"
+printf "\n** Warm cache **\n"
+wget \
+  --recursive \
+  --delete-after \
+  --level 0 \
+  --quiet \
+  "https://supergood.work/"
+
+printf "\n** Done **\n"
